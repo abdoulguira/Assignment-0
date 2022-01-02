ed0c82ff42eecf77b6763352fa46029090513b68
abdoulguira <73608549+abdoulguira@users.noreply.github.com>
2022-01-02 17:31:03 -0500

    Update sumOfAllEvenNumbers.js

--- 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
+++ 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
@@ -1,5 +1,17 @@
 function sumOfAllEvenNumbers(nums) {
   // Insert code here;
+  
+  let result=0;
+  for(let i=0; i< nums.length; i++){
+    let rest= nums[i]%2;
+
+    if(rest === 0){
+      result++;
+    }
+
+  }
+
+  return result;
 }
 
 // Do not edit this line;
