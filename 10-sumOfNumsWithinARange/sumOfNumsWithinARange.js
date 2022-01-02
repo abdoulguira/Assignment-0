8f6734e4e6686754ee2e9e7f3f4c6c4d3d5b7c02
abdoulguira <73608549+abdoulguira@users.noreply.github.com>
2022-01-02 18:06:01 -0500

    Update sumOfNumsWithinARange.js

--- 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
+++ 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
@@ -1,5 +1,17 @@
 function sumOfNumsWithinARange(nums, start, end) {
   // Insert code here;
+  
+  let result=0;
+  for(let i=0; i< nums.length; i++){
+    
+
+    if(nums[i]>=start && nums[i]<=end){
+      result++;
+    }
+
+  }
+
+  return result;
 }
 
 // Do not edit this line;
