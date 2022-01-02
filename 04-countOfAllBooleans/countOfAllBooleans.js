83bb2ae212f856031449e8234bfd3c7061a20c2a
abdoulguira <73608549+abdoulguira@users.noreply.github.com>
2022-01-02 17:11:39 -0500

    Update countOfAllBooleans.js

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,16 +1,7 @@
 function countOfAllBooleans(arr) {
   // Insert code here;
   
-  let result=0;
-  for(let i=0; i< arr.length; i++){
-
-    if(arr[i]=== true || arr[i]=== false){
-      result++;
-    }
-
-  }
-
-  return result;
+  if(typeof arr[i] === "boolean")
 }
 
 // Do not edit this line;
