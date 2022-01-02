ded26808bb8569a182cb883f2906123dea8e62d9
abdoulguira <73608549+abdoulguira@users.noreply.github.com>
2022-01-02 17:21:22 -0500

    Update countOfAllNumbersSmallerThanTarget.js

--- 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
+++ 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
@@ -1,5 +1,16 @@
 function countOfAllNumbersSmallerThanTarget(nums, target) {
   // Insert code here;
+  
+  let result=0;
+  for(let i=0; i< arr.length; i++){
+
+    if(arr[i] < target){
+      result++;
+    }
+
+  }
+
+  return result;
 }
 
 // Do not edit this line;
