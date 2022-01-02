f9dad0ede1b6307b9943c853851fedd91880dd07
abdoulguira <73608549+abdoulguira@users.noreply.github.com>
2022-01-02 16:30:35 -0500

    Update subtractFive.js

--- 02-subtractFive/subtractFive.js
+++ 02-subtractFive/subtractFive.js
@@ -1,5 +1,12 @@
 function subtractFive(num) {
   // Insert code here;
+  if(num >=5){
+    num = num - 5;
+  }
+  else{
+    console.log("Input should be greater than 5");
+  }
+  return num;
 }
 
 // Do not edit this line;
