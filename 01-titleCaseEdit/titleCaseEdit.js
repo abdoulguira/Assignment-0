e5d2785936cbfca32aad73e196c319123eedfe4e
abdoulguira <73608549+abdoulguira@users.noreply.github.com>
2022-01-02 16:03:35 -0500

    Update titleCaseEdit.js

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,6 +1,15 @@
 function titleCaseEdit(title) {
   // Insert code here;
-  console.log(title);
+  const stringArray= title.split(' ');
+  
+  let result='';
+   for(let i=0;i<stringArray.length;i++){
+    
+     result += stringArray[i].charAt(0).toUpperCase() + stringArray[i].slice(1,stringArray[i].length) + ' ';
+
+   }
+  return result;
+  
 }
 
 // Do not edit this line;
