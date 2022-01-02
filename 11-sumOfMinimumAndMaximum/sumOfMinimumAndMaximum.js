87c1256fb359a1ed7e3a09c0825806676b478b61
abdoulguira <73608549+abdoulguira@users.noreply.github.com>
2022-01-02 18:22:56 -0500

    Update sumOfMinimumAndMaximum.js

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,5 +1,12 @@
 function sumOfMinimumAndMaximum(nums) {
   // Insert code here;
+  
+  let result=0;
+  let sortedNums= nums.sort( function(a, b) {return a-b} );
+  
+  result =sortedNums[0] + sortedNums[nums.length-1];
+
+  return result;
 }
 
 // Do not edit this line;
