9e3afc5348c5e4a5f71be9e7a2bf3b13c407441a
abdoulguira <73608549+abdoulguira@users.noreply.github.com>
2022-01-02 22:10:30 -0500

    Update productOfAnyAmountOfNumbers.js

--- 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
+++ 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
@@ -1,5 +1,13 @@
 function productOfAnyAmountOfNumbers(...args) {
   // Insert code here;
+  
+  let arr=args;
+  let result=1;
+  for(let i=0; i<arr.length; i++){
+    result *= arr[i]; 
+  }
+  return result;
+  
 }
 
 // Do not edit this line;
