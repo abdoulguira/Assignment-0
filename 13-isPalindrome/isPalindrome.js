96d09f8244803c6ca04c0acfd68956267520c193
abdoulguira <73608549+abdoulguira@users.noreply.github.com>
2022-01-02 19:42:52 -0500

    Update isPalindrome.js

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,5 +1,16 @@
 function isPalindrome(word) {
   // Insert code here;
+  
+  if(word.length===0 || word.length===1){
+    return true;
+  }
+
+  if(word.charAt(0) === word.charAt(word.length-1)){
+    return isPalindrome(word.substring(1,word.length-1));
+
+  }
+
+  return false;
 }
 
 // Do not edit this line;
