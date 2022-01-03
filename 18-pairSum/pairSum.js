c795c018d7a3d6c16ec43811c40912fa0010247f
abdoulguira <73608549+abdoulguira@users.noreply.github.com>
2022-01-03 02:36:58 -0500

    Update pairSum.js

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,5 +1,17 @@
 function pairSum(nums, target) {
   // Insert code here;
+  
+  let result=0;
+  for(let i=0; i<nums.length-1; i++){
+
+    for(let j=i+1; j<nums.length; j++){
+      result = nums[i] + nums[j];
+      if(result === target) return true;
+
+    }
+
+  }
+  return false;
 }
 
 // Do not edit this line;
