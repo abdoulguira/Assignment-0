1268fc7ec336c3b66784503b05d808989a694eed
abdoulguira <73608549+abdoulguira@users.noreply.github.com>
2022-01-02 21:36:44 -0500

    Update frequencyCounter.js

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,28 @@
 function frequencyCounter(word) {
   // Insert code here;
+  
+  let arr=[];
+  var obj ={};
+  let count=0;
+
+  for(let i=0; i<word.length; i++){
+    if ( arr.indexOf(word[i]) === -1 ){
+      arr.push(word[i]);
+    } 
+
+  }
+
+   for(let i=0; i< arr.length; i++){
+
+     count=0;
+     for(let j=0; j< word.length; j++){
+       if(arr[i]===word[j]) count++;
+     }
+       
+    obj[arr[i]]=count;
+  }
+  
+  return obj;
 }
 
 // Do not edit this line;
