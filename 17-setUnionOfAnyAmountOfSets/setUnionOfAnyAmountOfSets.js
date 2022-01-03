07d364b9fd3669be8e48d459333f844f4dfb4d0d
abdoulguira <73608549+abdoulguira@users.noreply.github.com>
2022-01-03 02:15:41 -0500

    Update setUnionOfAnyAmountOfSets.js

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,5 +1,24 @@
 function setUnionOfAnyAmountOfSets(...args) {
   // Insert code here;
+  
+  var set = new Set();
+  let arr = args;
+  let result=[];
+  
+  let t=0;
+  for(let i=0; i<arr.length; i++){
+
+    for(let j=0; j<arr[i].length; j++){
+      result[t]=arr[i][j];
+      set.add(result[t]);
+      t++;
+    }
+   
+  }
+  
+   for (let value of set) alert(value);
+  
+  return set;
 }
 
 // Do not edit this line;
