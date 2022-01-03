a0db61c8855546b0e46c863e726104f4e9a882bf
abdoulguira <73608549+abdoulguira@users.noreply.github.com>
2022-01-02 21:48:31 -0500

    Update countDownSum.js

--- 15-countDownSum/countDownSum.js
+++ 15-countDownSum/countDownSum.js
@@ -1,6 +1,14 @@
 class MySolution {
   countDownSum(num) {
     // Insert code here;
+    
+    // the base cases
+    if(num===0) return 0;
+    if(num===1) return 1;
+    
+    // recurse
+    return countDownSum(num-1) + num;
+    
   }
 }
 
