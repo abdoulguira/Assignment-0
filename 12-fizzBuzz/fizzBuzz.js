7ce81e6faa199a3588f630f30580b820b42c2ca3
abdoulguira <73608549+abdoulguira@users.noreply.github.com>
2022-01-02 19:23:48 -0500

    Update fizzBuzz.js

--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -1,5 +1,32 @@
 function fizzBuzz(start, end) {
   // Insert code here;
+  
+  let arr=[];
+  for(let i=0; i<=end-1; i++){
+    arr[i]=start;
+    if(start%3 ===0 && start%5===0){
+      arr[i]= 'FizzBuzz';
+      
+    }
+    else if(start%5 ===0){
+      arr[i]= 'Buzz';
+  
+    }
+    else if(start%3===0){
+      arr[i]= 'Fizz';
+     
+    }
+    else{
+      arr[i]= start;
+    }
+
+    start++;
+    
+  }
+
+  return arr;
+  
+
 }
 
 // Do not edit this line;
